e470fb1365793195c6355fb7db048b5b0bce47e7
Ky Bluff <8600765+CodeSuite@users.noreply.github.com>
2024-01-05 13:34:42 -0700

    Mobile view considerations.

--- CookieConsent.vue
+++ CookieConsent.vue
@@ -43,16 +43,16 @@
     <div v-if="showDialog" class="fixed bottom-0 left-0 right-0 inset-x-0 pb-2 z-50">
         <div class="w-full max-w-7xl mx-auto px-6">
             <div class="p-2 rounded-lg bg-black border border-white">
-                <div class="grid grid-flow-col place-items-center justify-between flex-wrap">
-                    <div class="w-full flex-1 items-center hidden md:block">
-                        <p class="ml-3 text-white cookie-consent__message">
+                <div class="grid grid-cols-1 lg:grid-flow-col place-items-center justify-between">
+                    <div class="w-full items-center">
+                        <p class="ml-3 text-white">
                             This app uses cookies to track and enhance your experience on this site.
                         </p>
                     </div>
-                    <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
+                    <div class="mt-2 w-full sm:mt-0 sm:w-auto">
                         <button
                             @click="consentWithCookies"
-                            class="cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-black bg-white">
+                            class="w-full mt-5 lg:mt-auto cursor-pointer px-4 py-2 rounded-md text-sm font-medium text-black bg-white">
                             Allow cookies
                         </button>
                     </div>
